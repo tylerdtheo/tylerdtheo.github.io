2c07e6924cc9a081cda6cb933fc82c25ac3e3b3a
tylerdtheo <82241166+tylerdtheo@users.noreply.github.com>
2021-04-09 22:55:40 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -23,9 +23,10 @@ $.getJSON(weatherAlertsUrl, function(data) {
       var alertColor = 'orange';
       if (feature.properties.severity === 'Severe') alertColor = 'red';
       return { color: alertColor }
+      
        style: function(feature){
-      var alertColor = 'pink';
-      if (feature.properties.severity === 'Extreme') alertColor = 'orange';
+      var alertColor = 'orange';
+      if (feature.properties.severity === 'Extreme') alertColor = 'pink';
       return { color: alertColor
     },
     // Add a popup on each feature showing the NWS alert headline
