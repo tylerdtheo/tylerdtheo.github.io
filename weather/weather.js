c487186f274e49cc72ccb90bb70e14ca15d61833
tylerdtheo <82241166+tylerdtheo@users.noreply.github.com>
2021-04-09 23:10:35 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -22,7 +22,8 @@ $.getJSON(weatherAlertsUrl, function(data) {
     style: function(feature){
       var alertColor = 'orange';
       if (feature.properties.severity === 'Severe') alertColor = 'red';
-      if (feature.properties.severity === 'Minor') alertColor = 'blue';
+      if (feature.properties.severity === 'Minor') alertColor = 'green';
+      if (feature.properties.severity === 'Extreme') alertColor = 'pink';
       return { color: alertColor
        style: function(feature){
     },
