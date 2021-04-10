76b514e380573e449379ad6f4636d5102fc42319
tylerdtheo <82241166+tylerdtheo@users.noreply.github.com>
2021-04-09 20:03:12 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -8,3 +8,8 @@ var radarDisplayOptions = {
   transparent: true
 };
 var radar = L.tileLayer.wms(radarUrl, radarDisplayOptions).addTo(map);
+var weatherAlertsUrl = 'https://api.weather.gov/alerts/active?region_type=land';
+$.getJSON(weatherAlertsUrl, function(data) {
+  L.geoJSON(data).addTo(map);
+
+});
