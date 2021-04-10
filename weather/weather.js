08addcc710401a7d21369aea7fccb4056d390cab
tylerdtheo <82241166+tylerdtheo@users.noreply.github.com>
2021-04-09 19:59:12 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -1,3 +1,10 @@
 var map = L.map('map').setView([38, -95], 4);
 var basemapUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 var basemap = L.tileLayer(basemapUrl).addTo(map);
+var radarUrl = 'https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi';
+var radarDisplayOptions = {
+  layers: 'nexrad-n0r-900913',
+  format: 'image/png',
+  transparent: true
+};
+var radar = L.tileLayer.wms(radarUrl, radarDisplayOptions).addTo(map);
